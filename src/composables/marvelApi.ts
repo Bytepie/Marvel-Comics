1aadd59433a1b4c7cdb610a3bb6e5f3b25440341
DESKTOP\Umer Tahir <umertahir6@gmail.com>
2024-07-14 17:18:23 +0500

    update

--- src/composables/marvelApi.ts
+++ src/composables/marvelApi.ts
@@ -1,4 +1,4 @@
-import type { Comics, Path } from "@/types/marvel";
+import type { Characters, Comics, Path } from "@/types/marvel";
 
 const apiKey = import.meta.env.VITE_APP_MARVEL_API_PUBLIC;
 const MARVEL_API = "//gateway.marvel.com/v1/public/";
@@ -6,11 +6,7 @@ const API_SIGN = `apikey=${apiKey}`;
 const ITEMS_PER_PAGE = 20;
 
 export const useComics = async (page: number = 0): Promise<Comics> => {
-  const pagination = page ? `&offset=${page * ITEMS_PER_PAGE}` : "";
-  const requestURI = `${MARVEL_API}/comics?${API_SIGN}${pagination}`;
-  const result = await fetch(requestURI);
-  const jsonResult = await result.json();
-  return jsonResult.data;
+  return await useMarvelApi(Path.COMICS, { page });
 };
 
 const getPagination = (page?: number): string => {
@@ -28,4 +24,27 @@ const getRequestURI = (
 ): string => {
   const apiPath = `${MARVEL_API}/${path}`;
   return `${apiPath}${API_SIGN}${query}${pagination}`;
+};
+
+export const useFetch = async (
+  requestURI: string
+): Promise<Comics | Characters> => {
+  const result = await fetch(requestURI);
+  const jsonResult = await result.json();
+  return jsonResult.data as Comics | Characters;
+};
+
+interface ApiOptions {
+  query?: string;
+  page?: number;
+}
+
+export const useMarvelApi = async (
+  path: Path,
+  options: ApiOptions
+): Promise<Comics | Characters> => {
+  const pagination = getPagination(options.page);
+  const query = getQuery(options.query);
+  const requestURI = getRequestURI(path, query, pagination);
+  return useFetch(requestURI);
 };
